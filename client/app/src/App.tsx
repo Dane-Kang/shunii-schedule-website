2e35070b101d74267efea76b25eb42d3afad1e45
Dane-Kang <dane.kang@realtek.com>
2024-12-26 14:09:16 +0900

    Implemented DatePicker

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,16 +1,7 @@
-import {
-  Header,
-  Contact,
-  VisitorCounter,
-  VisitorComment,
-} from "dev-portfolio";
 
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import useComment from "./hooks/useComment";
 import "./App.css";
-import COLOR from "./common/style/theme";
-import countAPI from "./apis/count";
 
 import MyCalendar from './MyCalendar';
 import { EventInput } from '@fullcalendar/core';
@@ -47,56 +38,6 @@ function App() {
 
   return (
     <div className="App">
-      <Header
-        headerBackgroundColor={COLOR.POINT_COLOR}
-        logoOption={{
-          redirectUrl: "/",
-          title: "Monthly Scheduler",
-          logoImg: "./박우림_일러스트.png",
-          // logoHidden: true,
-          titleColor: `${COLOR.CLEAN_BLUE}`,
-          titleSize: "24px",
-        }}
-        channels={[
-          { name: "github", redirectUrl: "/", color: `black`, size: "1px", margin: "0 1px 0 0",},
-          { name: "linkedin", redirectUrl:"/", color: `black`, size: "1px", margin: "0 1px 0 0",},
-          { name: "youtube", redirectUrl:"https://www.youtube.com/@yokkang4661", color: `${COLOR.CLEAN_BLUE}`, size: "26px", margin: "0 1px 0 0", },
-        ]}
-        sideBarOption={{
-          mainTitle: "Management Member :D",
-          mainTitleSize: "20px",
-          mainTitleColor: "white",
-          mainTitleAlign: "left",
-          mainTitleBorderColor: "white",
-          iconName: "ant-design:menu-fold-outlined", //Refer to the guidelines.
-          iconSize: "30px",
-          iconColor: `${COLOR.MAIN_COLOR}`,
-          iconMargin: "0px 12px",
-          itemTextColor: `${COLOR.MAIN_COLOR}`,
-          itemTextAlign: "left",
-          itemBackgroundColor: `${COLOR.POINT_COLOR}`,
-          itemHoverdBackgroundColor: `${COLOR.CLEAN_BLUE}`,
-          backgroundColor: `${COLOR.POINT_COLOR}`,
-        }}
-      />
-
-      {/* <VisitorCounter
-        theme="big-size"
-        backgroundColor={COLOR.MAIN_COLOR}
-        size="14px"
-        title="조회수"
-        titleColor={COLOR.POINT_COLOR}
-        todayTitle="오늘"
-        todayTitleColor={COLOR.POINT_COLOR}
-        todayVisitor={todayCounter}
-        todayVisitorColor={COLOR.CLEAN_BLUE}
-        totalTitle="전체"
-        totalTitleColor={COLOR.POINT_COLOR}
-        totalVisitor={totalCounter}
-        totalVisitorColor={COLOR.CLEAN_BLUE}
-      /> */}
-
-      <UnderLine></UnderLine>
 
       <MyCalendar events={events} />
       <Table
@@ -131,62 +72,8 @@ function App() {
         handleChangePassword={handleChangePassword} // Event handling variable
       /> */}
 
-      <Contact
-        title="Schedule Calendar"
-        titleColor={COLOR.CLEAN_BLUE}
-        backgroundColor={COLOR.POINT_COLOR}
-        channels={[
-          {
-            name: "youtube",
-            redirectUrl:
-              "https://www.youtube.com/@yokkang4661",
-            color: `${COLOR.CLEAN_BLUE}`,
-            margin: "0 10px 0 0",
-          },
-        ]}
-        aboutMeInfos={[
-          {
-            title: "이 메 일",
-            titleColor: `${COLOR.CLEAN_BLUE}`,
-            description: "kangyosoon1636@gmail.com",
-            descriptionColor: `${COLOR.MAIN_COLOR}`,
-          },
-        ]}
-      />
     </div>
   );
 }
 
 export default App;
-
-const ContactTitle = styled.h1`
-  margin: 0;
-  padding: 0 1em 0 1em;
-`;
-
-const FavoriteText = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  font-weight: 600;
-  padding: 10px 20px;
-  background-color: ${COLOR.MAIN_COLOR};
-  border-radius: 8px;
-`;
-
-const SkillContainer = styled.div`
-  display: flex;
-  direction: row;
-  margin: 2em 2em 0 2em;
-`;
-
-const SkillBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-`;
-
-const UnderLine = styled.hr`
-  border: 0.5px dashed ${COLOR.SIMPLE_GREY};
-`;
--- client/app/src/ReactTable.tsx
+++ client/app/src/ReactTable.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import useAgent from "./hooks/useAgentinfo";
+import DatePicker from 'react-multi-date-picker';
+import "react-multi-date-picker/styles/layouts/mobile.css";
+import "react-multi-date-picker/styles/colors/green.css";
+import { DateObject } from 'react-multi-date-picker'; // DateObject를 임포트
 import './styles.css';
+import Day from 'react-datepicker/dist/day';
 
 export interface Agent {
   name: string;
@@ -25,6 +30,8 @@ const Table: React.FC = () => {
   } = useAgent();
 
   const [data, setData] = useState<Agent[]>([]); // Agent[] 타입으로 초기화
+  const [selectedDates, setSelectedDates] = useState<{ [key: number]: DateObject[] }>({});
+
   // 수동으로 열 너비 설정
   const columnWidths = [60, 160, 300];
   // Header 이름 설정
@@ -32,12 +39,42 @@ const Table: React.FC = () => {
   // 수동으로 열 수정 가능 여부 설정
   const editableColumns = [true, true, true, true];
 
+  const getDescriptionValue = (value: any): Date[] => {
+    if (Array.isArray(value)) {
+      return value;
+    }
+    return [];
+  };
+
   // agentList를 기반으로 데이터 설정
   useEffect(() => {
     if (agentList) {
-      const filteredData = agentList.map(({id, ...rest }) => rest); // id를 제외한 데이터로 변환
-      console.log('filteredData :',filteredData);
-      setData(filteredData);
+      const filteredinfo = agentList.map(({id, ...rest}) => rest); // id를 제외한 데이터로 변환
+      console.log('filteredinfo :',filteredinfo);
+      setData(filteredinfo);
+
+      const selectedDatesMapping: { [key: number]: DateObject[] } = {};
+
+      let rowid: number = 0;
+      filteredinfo.forEach((agent) => {
+        const description = agent.description;
+
+        const serverDates = description.split(',').map((date: string) => date.trim());  // 공백을 제거하고 배열로 변환
+        console.log('foreach serverDates :',serverDates);
+
+        const dateObjects = serverDates.map((dateStr: string) => {
+          const trimmedDateStr = dateStr.trim(); // 공백 제거
+          const [year, month, day] = trimmedDateStr.split('-').map(Number);
+          return new DateObject({ year, month, day });
+        });
+        console.log('selecetdata :',dateObjects);
+
+        selectedDatesMapping[rowid] = dateObjects;
+        rowid++;
+      });
+
+      // selectedDates 업데이트
+      setSelectedDates(selectedDatesMapping);
     }
   }, [agentList]);
 
@@ -66,6 +103,30 @@ const Table: React.FC = () => {
         //setData(newData);
       }
     }
+  };
+
+  const handleDateChange = (rowIndex: number, dates: DateObject[]) => {
+    selectedDates[rowIndex] = dates;
+
+    setSelectedDates(selectedDates);
+    console.log(dates);
+  };
+
+  const handleDatePickerClose = (rowIndex: number) => {
+    // DatePicker가 닫힐 때 호출되는 함수
+    // 선택된 날짜들을 처리
+    const formattedDates = selectedDates[rowIndex].map(date => {
+        const dateInstance = date.toDate();
+        const year = dateInstance.getFullYear();
+        const month = dateInstance.getMonth() + 1;
+        const day = dateInstance.getDate();
+        return `${year}-${month}-${day}`; // 월/일 형식으로 변환
+      })
+      .join(", "); // 여러 날짜들을 쉼표로 구분하여 연결
+
+    console.log("handleDatePickerClose: ", formattedDates); // 선택된 날짜들 출력
+    // 상태를 처리하는 함수 호출
+    handleInputChange(rowIndex, 'description', formattedDates);
   };
 
   const handleInputChange = (rowIndex: number, field: keyof Agent, value: string) => {
@@ -134,13 +195,25 @@ const Table: React.FC = () => {
               </td>
               {['name', 'job_level', 'description'].map((field, colIndex) => (
                 <td key={colIndex} style={{ width: `${columnWidths[colIndex]}px` }}>
+                {field === 'description' ? (
+                  <DatePicker
+                    onChange={(dates: DateObject[]) => handleDateChange(rowIndex, dates)}
+                    onClose={() => handleDatePickerClose(rowIndex)}
+                    value={selectedDates[rowIndex]}
+                    multiple
+                    readOnly={!editableColumns[colIndex]}
+                    format="MM/DD"
+                    calendarPosition="bottom-center"
+                    className="black"
+                  />
+                ) : (
                   <input
                     type="text"
-                    // value={row[field as keyof Omit<Agent, 'isNew'>] as string}
                     value={row[field as keyof Agent] as string}
                     onChange={(e) => handleInputChange(rowIndex, field as keyof Agent, e.target.value)}
                     readOnly={!editableColumns[colIndex]}
                   />
+                )}
                 </td>
               ))}
               {row.isNew && (
